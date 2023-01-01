7a0d9d2bfa7b4b538dc0a0767f03c6abe746cb4a
Suryakant_2003 <32999024+GivenBY@users.noreply.github.com>
2023-01-01 04:29:38 +0000

    thanks

--- Sealer.py
+++ Sealer.py
@@ -35,18 +35,35 @@ def pdf_jpg(file,wmark,Watermark):
         if wmark==("y" or "Y"):
             imgWatermark(("../OutputJpg/page-%i.jpg" % page.number),Watermark)        
 
-def jpg_pdf():
-    images = [Image.open(f) for f in os.listdir("../jpg-pdf")]
+def jpg_pdf(file):
+    images = [Image.open(f) for f in os.listdir(f"../{file}")]
     pdf_path = "/"
     images[0].save(pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
 
 while True:
-    print('''To add waterMark to Single Jpg File >> W''')
-    print('''To Extract Jpg From Pdf File and Add WaterMark >> k''')
-    print('''To add waterMark to Single File >> W''')
+    print('''To add waterMark to Single Jpg File >> W/w''')
+    print('''To Extract Jpg From Pdf File and Add WaterMark >> J/j''')
+    print('''To Extract Pdf File From Jpg >> A/a''')
+    print('''Press Another Key to Escape >>''')
     k=input("Enter >> ")
-
-
-jpg_pdf()
-pdf_jpg()
-imgWatermark()
+    if k=="W" or k=="w":
+        file=input("Enter File Name : ")
+        watmark=input("Enter WaterMark")
+        imgWatermark(file,watmark)
+    elif k=="J" or k=="j":
+        print('''Do you want to add add WaterMark >>''')
+        print('''Press y/y for Yes else any key for No''')
+        c=input("Input Choice : ")
+        if c=="y" or c=="Y":
+            file=input("Enter File Name : ")
+            watmark=input("Enter WaterMark")
+            pdf_jpg(file,watmark,c)
+        else:
+            file=input("Enter File Name : ")
+            watmark=input("Enter WaterMark")
+            pdf_jpg(file,watmark,"No")
+    elif k=="A" or k=="a":
+        file=input("Enter File Name : ")
+        jpg_pdf(file)
+    else:
+        break
